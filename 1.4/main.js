ef2c86c4a7dc961d1ca10c2990a7b3cc8d78325a
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-10-06 19:59:47 +0200

    forth lesson done

---
+++ 1.4/main.js
@@ -0,0 +1,29 @@
+//1.4.1
+// var VALUE = 'hello world';
+// VALUE = 'foo bar';
+// console.log('value: ', VALUE);
+
+// const VALUE = 'hello world';
+// VALUE = 'foo bar';
+// console.log('value: ', VALUE);
+
+//1.4.2
+// const VALUE = {};
+// VALUE.foo = 'bar';
+// console.log('value: ', VALUE);
+
+// const VALUE = {};
+// VALUE = 'bar';
+// console.log('value: ', VALUE);
+
+//1.4.3
+// if (true) {
+//   const foo = 'bar';
+// }
+//
+// console.log('foo: ', foo);
+
+if (true) {
+  const foo = 'bar';
+  console.log('foo: ', foo);
+}
